fe8c522c62a3e49e6400ee18c10d1fc899f1b498
Ahmed Ayman <ahmedaymann1411@gmail.com>
2025-04-13 21:16:53 +0200

    new_fathure

--- gradution_project/lib/feathure/on_boarding/splash_screen.dart
+++ gradution_project/lib/feathure/on_boarding/splash_screen.dart
@@ -11,10 +11,42 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+
   @override
   void initState() {
     super.initState();
+
+    // أنيميشن التحكم
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1200),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0, 1), // تبدأ من تحت
+      end: Offset.zero, // وتنتهي في النص
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutBack,
+    ));
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    ));
+
+    // نبدأ الأنيميشن
+    _controller.forward();
+
+    // بعد 3 ثواني نروح على StartPage
     Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
@@ -24,15 +56,42 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
+  void dispose() {
+    _controller.dispose(); // لازم نعمل ديسبوز
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.black,
       body: Center(
-          child: Image.asset(
-        AppAssets.mainlogo,
-        width: 200,
-        height: 200,
-      )),
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: SlideTransition(
+            position: _slideAnimation,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  AppAssets.mainlogo,
+                  width: 200,
+                  height: 200,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'WeyNak',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontFamily: 'Concert One'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
